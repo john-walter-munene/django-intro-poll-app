35ede2ce699228dbab6917811c50833a10eea0c1
Munene254 <midwaymaster10@gmail.com>
2022-10-13 04:57:45 +0300

    Good with writing Views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from re import template
+from django.http import Http404, HttpResponse
+from .models import Question
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def index(request):
     """Declare my home page"""
-    return HttpResponse("Hello friend, you are at the poll index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_quetsion_list': latest_question_list
+    }
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return render(request, 'polls/index.html', context)
 
+def detail(request, question_id):
+    """Display a question with no results but a form to vote"""
+    """
+    try: 
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    
+
+def results(request, question_id):
+    """Display results for a particular question"""
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    """Handles voting for particular choice in particular question."""
+    return HttpResponse("You're voting on question %s." % question_id)
