1ca43292e670b032f1bb79b0222a083f2ae9f2f7
Munene254 <midwaymaster10@gmail.com>
2022-10-25 04:39:24 +0300

    Completed django polls apk, can now move to writing reusable apps

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 from django.db import models
 import datetime
+from django.contrib import admin
 
 # Create your models here.
 
@@ -19,13 +20,22 @@ class Question(models.Model):
         return now - datetime.timedelta(days=1) <=self.pub_date <= now 
         # Commented out of the bug after testing. 
         # self.pub_date >= timezone.now() - datetime.timedelta(days=1) 
-    
-    
+       
 class Choice(models.Model):
     """The Choices and Tallying votes."""
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
     def __str__(self):
         """Create a better visual impression"""
